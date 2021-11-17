71be9f39c3632cec385571d081b607223f9031d3
Elisangelo Alves Ferreira <elisangelo@Mac-mini-de-Elisangelo.local>
2021-11-17 00:37:30 -0300

    implementacao da classes de servicos do Category e interface

--- src/main/java/com/br/java/carteiradigital/service/serviceimpl/CategoryServiceImpl.java
+++ src/main/java/com/br/java/carteiradigital/service/serviceimpl/CategoryServiceImpl.java
@@ -1,2 +1,32 @@
-package com.br.java.carteiradigital.service.serviceimpl;public class CategoryServiceImpl {
+package com.br.java.carteiradigital.service.serviceimpl;
+
+import com.br.java.carteiradigital.controller.response.CategoryResponse;
+import com.br.java.carteiradigital.model.Category;
+import com.br.java.carteiradigital.repository.CategoryRepository;
+import com.br.java.carteiradigital.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public List<CategoryResponse> listCategories() {
+        List<Category> categories = categoryRepository.findAll();
+        return CategoryResponse.listCategories(categories);
+    }
+
+    @Override
+    public CategoryResponse createCategory(Category category) {
+        Category newCategory = categoryRepository.save(category);
+        return new CategoryResponse(newCategory);
+    }
 }
