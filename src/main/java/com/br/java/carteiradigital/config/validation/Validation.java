c1ecf22aba9e1944428530c509cd2cd10560f6c6
elisangeloalves <preto115@gmail.com>
2021-11-20 23:45:39 -0300

    implementando logica para validar se o recurso acessodo pertence ao usuario loggado

--- src/main/java/com/br/java/carteiradigital/config/validation/Validation.java
+++ src/main/java/com/br/java/carteiradigital/config/validation/Validation.java
@@ -2,12 +2,15 @@ package com.br.java.carteiradigital.config.validation;
 
 import com.br.java.carteiradigital.config.validation.ApiErrorException;
 import com.br.java.carteiradigital.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 
+@Slf4j
 public class Validation {
 
     private Validation() {
+        log.info("Tenttiva de acesso negado.");
         throw new ApiErrorException("Access Denied",
                 "You don`t have permission to access this resource.",
                 HttpStatus.FORBIDDEN);
